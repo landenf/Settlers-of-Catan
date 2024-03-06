3ea08b82e21bb02e237321875792462d6ef06619
ebelashchenko <ebelashchenko2@huskers.unl.edu>
2024-03-05 23:55:24 -0600

    resource distribution written

---
+++ server/src/gameplay.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+/**
+ * Function for distributing resources to the players based on the number rolled.
+ * NOTE: This function may have to change depending on what what data types is in the players function!
+ * 
+ * @param {*} players list of players in the game
+ * @param {int} numRolled the number rolled
+ */
+function distributeCards(players, numRolled) {
+     for(let i = 0; i < players.length; i++){
+          const map = players[i].getResources(numRolled);
+          for(let j = 0; j < map.length; j++){
+               players.resources.get(map[i]) = players.resources.get(map[i]) + 1;
+          }
+     }
+}
