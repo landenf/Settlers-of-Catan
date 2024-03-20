db05cabb3552e906f932f6e8562fa81f1dbf54e3
SirFatredVIII <dhall25@huskers.unl.edu>
2024-03-20 13:13:13 -0500

    Updated server
    Fixed an error in which the websocket would prematurely terminate connection

--- server/server.js
+++ server/server.js
@@ -12,24 +12,11 @@ const server = app.listen(port, () => {console.log("Server Started")} );
 
 const wss = new WebSocket.Server({  server: server  });
 
-server.on('upgrade', (req, socket, head) => {
-    // perform auth
-    if (!req.headers['BadAuth']) {
-        socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n');
-        socket.destroy();
-        return;
-    }
-
-    wss.handleUpgrade(req, socket, head, (ws) => {
-        wss.emit('connection', ws, req);
-    });
-});
-
 wss.on('connection', (ws, req) => {
     ws.send("hello!");
     ws.on('message', (msg, isBinary) => {
         // goes through each client and sends it
-        wss.clients.forEach(() => {
+        wss.clients.forEach((client) => {
             // checks if it is a different client and if the client is ready
             // to send to everyone, including yourself, take off ws !== client
             if(ws !== client && client.readyState === WebSocket.OPEN) {
