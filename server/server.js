14c94d83f5674f08be5370d95b50f6b560de5374
ebelashchenko <ebelashchenko2@huskers.unl.edu>
2024-03-21 17:36:10 -0500

    update server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const gameplay = require("./src/gameplay.js")
-const express = require('express');
-const app = express();
-const port = 5000;
-const WebSocket = require('ws');
+const express = require('express')
+const app = express()
 const cors = require("cors");
 
 app.use(express.json())
@@ -11,6 +9,10 @@ app.use(cors("*"))
 
 app.get("/test", (req, res) => {
     res.json({"users": ["userone","usertwo"]})
+})
+
+app.post("/buyDevCard", (req, res) => {
+    gameplay.buyDevCard(req.body);
 })
 
 app.listen(5000, () => {console.log("Server Started")} )
