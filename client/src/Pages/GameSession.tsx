343f4aa5e64d4e258250c36f12f52988948b6496
SirFatredVIII <dhall25@huskers.unl.edu>
2024-03-21 15:20:06 -0500

    Added victory points component
    + Added victory points component to the bottom of the screen

--- client/src/Pages/GameSession.tsx
+++ client/src/Pages/GameSession.tsx
@@ -2,6 +2,7 @@ import GameBoard from "../Components/GameBoard";
 import PlayerBarComponent from "../Components/PlayerBarComponent";
 import ActionsBarComponent from "../Components/ActionsBarComponent";
 import Hand from "../Components/Hand"
+import VictoryPointsComponent from "../Components/victoryPointsComponent";
 import React from "react";
 import { StateProp } from "../Components/types";
 import { players } from "../StaticData/PlayerData";
@@ -15,7 +16,10 @@ const GameSession = (props: StateProp) => {
                 <div className="PlayerbarComponent"><PlayerBarComponent players={players}/></div>
                 <div className="center-column">
                     <div className="game-board"><GameBoard tiles={tiles}/></div>
-                    <Hand/>
+                    <div className="user-info">
+                      <VictoryPointsComponent/>
+                      <Hand/>
+                    </div>
                 </div>
                 <div className="ActionsBarComponent"><ActionsBarComponent/></div>
             </div>
