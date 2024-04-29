4d409542ae19db17c55d83b160b8038838646e68
scunningham8 <scunningham8@huskers.unl.edu>
2024-04-29 13:40:58 -0500

    Statistics Styling Done

--- client/src/Components/PlayerStatsComponent.tsx
+++ client/src/Components/PlayerStatsComponent.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import "../Styles/PlayerStats.css";
 import "../Styles/JoinRoomWithCode.css";
 import { LimitedPlayer, Player } from "@shared/types";
-const PlayerStatsComponent = (player: LimitedPlayer) => {
+const PlayerStatsComponent = (player: Player) => {
   const player_image = `/images/${player.image}.jpg`;
   return (
     <div className="Profile">
       <div className="main-content-box">
         <img className="avatar" src={player_image} alt="Avatar Image" />
-        <div className="stats"></div>
         <div className="playerName">STEVE</div>
+        <div className="stats">
+          {/* {player.player_stats.map((stat: number) => {
+            <p className="individual-stat">{stat}</p>;
+          })} */}
+          <p className="individual-stat">total wins: 2</p>
+          <p className="individual-stat">total wins: 2</p>
+          <p className="individual-stat">total wins: 2</p>
+          <p className="individual-stat">total wins: 2</p>
+        </div>
       </div>
     </div>
   );
--- client/src/Pages/LandingPage.tsx
+++ client/src/Pages/LandingPage.tsx
@@ -31,6 +31,97 @@ const LandingPage: React.FC = () => {
         color={players[0].color}
         vp={players[0].vp}
         resources={players[0].resources}
+        hand={{
+          wheat: 0,
+          brick: 0,
+          stone: 0,
+          sheep: 0,
+          wood: 0,
+        }}
+        hasKnight={false}
+        knightCards={0}
+        resource_gain={{
+          2: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          3: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          4: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          5: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          6: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          8: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          9: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          10: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          11: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+          12: {
+            wheat: 0,
+            brick: 0,
+            stone: 0,
+            sheep: 0,
+            wood: 0,
+          },
+        }}
+        communities_owned={[]}
+        potential_communities={[]}
+        roads_owned={[]}
+        potential_roads={[]}
+        player_stats={{
+          total_wins: 0,
+          largest_armies: 0,
+          most_roads: 0,
+          total_vp: 0,
+        }}
       />
     </div>
   );
